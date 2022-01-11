7fd841bb4b189c7eeef9407ce0ee730497a1b0a0
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2022-01-11 14:06:43 -0600

    initial

---
+++ src/labs/Lab03.java
@@ -0,0 +1,35 @@
+package labs;
+
+public class Lab03 {
+
+	
+	public static void main(String[] args) {
+
+		problem01();
+		problem02();
+		problem03();
+
+	}
+
+
+	public static void problem01() {
+
+
+	}
+
+
+	public static void problem02() {
+
+
+	}
+
+
+	public static void problem03() {
+
+
+	}
+
+	
+	
+	
+}
